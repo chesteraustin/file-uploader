ac7b116a69967db03d03a931eb3ca67172d0f20e
chesteraustin <chesteraustin@gmail.com>
2023-03-21 14:04:40 -0700

    create new Home controller

--- file-uploader/Controllers/HomeController.cs
+++ file-uploader/Controllers/HomeController.cs
@@ -1,22 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace file_uploader.Controllers
 {
+    [Route("api/[controller]")]
     [ApiController]
-    [Route("[controller]")]
     public class HomeController : ControllerBase
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        // GET: api/Home
+        [HttpGet]
+        public IEnumerable<string> Get()
         {
-            _logger = logger;
+            return new string[] { "value1", "value2" };
         }
 
-        [HttpGet(Name = "HomeEndpoint")]
-        public IActionResult Get()
+        // GET api/Home/5
+        [HttpGet("{id}")]
+        public string Get(int id)
         {
-            return Ok("Hello World!!!");
+            return "value";
+        }
+
+        // POST api/Home
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/Home/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/Home/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
         }
     }
 }
